6af783798ad6cecb7def17b8541fa9a8f311cf97
Joeva-code <aduguj@gmail.com>
2026-05-18 14:04:18 +0100

    Fixed deployment issues

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ import cors from "cors";
 import todoRoutes from "./routes/todo.js";
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/todo", todoRoutes);
 
-app.listen(5000, () => {
-    console.log("Server running on port 5000");
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
