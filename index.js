0ecc99f8f0c29113d2bbdf976637fffef970c5a8
Joeva-code <aduguj@gmail.com>
2026-05-19 06:39:18 +0100

    Ready for Prod!

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import todoRoutes from "./routes/todo.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
